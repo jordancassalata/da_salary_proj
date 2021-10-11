5c121408ae89332f8ecfbd08c1a3223391b69633
jordancassalata <jordan.cassalata@gmail.com>
2021-10-10 20:56:36 -0700

    scapped data

---
+++ data_collection.py
@@ -0,0 +1,13 @@
+"""
+Created on Thu Apr  2 11:47:44 2020
+@author: Ken
+"""
+
+import glassdoor_scraper as gs 
+import pandas as pd 
+
+path = "C:/Users/jjcas/OneDrive/Documents/da_salary_proj/chromedriver"
+
+df = gs.get_jobs('data analyst',1000, False, path, 15)
+
+df.to_csv('glassdoor_jobs.csv', index = False)
