5541ad6df14bcdfeec181025c1595cab195e979a
jordancassalata <jordan.cassalata@gmail.com>
2021-10-11 20:07:56 -0700

    fix scraper

--- data_collection.py
+++ data_collection.py
@@ -8,6 +8,6 @@ import pandas as pd
 
 path = "C:/Users/jjcas/OneDrive/Documents/da_salary_proj/chromedriver"
 
-df = gs.get_jobs('data analyst',1000, False, path, 15)
+df = gs.get_jobs('data analyst',900, False, path, 15)
 
 df.to_csv('glassdoor_jobs.csv', index = False)
